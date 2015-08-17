6ce31ef3101f902750acb04b18135c47166a07bf
zhangkai365 <zhangkai365@live.cn>
2015-08-17 22:51:10 +0800

    Version:0.00.0001
    *nothing but the main framework.

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>天津市抗癫痫协会</title>
+	<link rel="defaultstyle" type="text/css" href="CSS/Default/Default.css"></link>
+</head>
+<body>
+	<div class="outerheaderlogin">
+		<div class="headerlogin">
+			<p id="headerloginwelcom">您好，欢迎来到天津抗癫痫协会！今天是：</p>
+		</div>
+	</div>
+	<div class="outerheaderlogo">
+		<div class="headerlogo"></div>
+	</div>
+	<div class="nav">
+	</div>
+	<div class="main">
+	</div>
+	<div class="footer">
+	</div>
+</body>
+</html>
